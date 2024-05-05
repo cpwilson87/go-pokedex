a63f4496634ba6613c3ba76b5c474fb3b825598a
Chris Wilson <cpwilson87@outlook.com>
2024-05-05 11:30:06 +0100

    feat: add cache

--- internal/pokeapi/location_area_request.go
+++ internal/pokeapi/location_area_request.go
@@ -10,8 +10,22 @@ import (
 func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResponse, error) {
 	endpoint := "/location-area"
 	fullURL := baseURL + endpoint
+
 	if pageURL != nil {
 		fullURL = *pageURL
+	}
+
+	data, ok := c.cache.Get(fullURL)
+	if ok {
+		// cache hit
+		locationAreasResponse := LocationAreasResponse{}
+		error := json.Unmarshal(data, &locationAreasResponse)
+		if error != nil {
+			return LocationAreasResponse{}, error
+		}
+
+		return locationAreasResponse, nil
+
 	}
 
 	request, error := http.NewRequest("GET", fullURL, nil)
@@ -28,7 +42,7 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResponse, erro
 		return LocationAreasResponse{}, fmt.Errorf("bad status code: %v", response.StatusCode)
 	}
 
-	data, error := io.ReadAll(response.Body)
+	data, error = io.ReadAll(response.Body)
 	if error != nil {
 		return LocationAreasResponse{}, error
 	}
@@ -38,7 +52,7 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResponse, erro
 	if error != nil {
 		return LocationAreasResponse{}, error
 	}
+	c.cache.Add(fullURL, data)
 
 	return locationAreasResponse, nil
-
 }
